0494cfc5121be5d8dbeef0ae73c5e93d7c1c4090
Sofya <shesofya@gmail.com>
2021-02-04 15:25:15 +0300

    Add routes to AppSwitch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Login from "./Components/Login/Login";
-import Main from "./Components/Main/Main";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppSwitch from "./Components/Utilities/AppSwitch";
 import Navbar from "./Components/Navbar/Navbar";
-import Register from "./Components/Register/Register";
 
 function App() {
   return (
     <div className="container">
       <Router>
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-        </Switch>
+        <AppSwitch />
       </Router>
     </div>
   );
--- src/Components/Utilities/AppSwitch/AppSwitch.jsx
+++ src/Components/Utilities/AppSwitch/AppSwitch.jsx
@@ -1,9 +1,38 @@
-const AppSwitch = () => {
-	return (
-		<div>
-			This is AppSwitch
-		</div>
-	)
-}
+import { Route, Switch } from "react-router-dom";
 
-export default AppSwitch
+const AppSwitch = () => {
+  return (
+    <Switch>
+			<Route path="/account">
+				{/* личный кабинет */}
+      </Route>
+			<Route path="/promise/new">
+        {/* создание новой цели */}
+      </Route>
+			<Route path="/friends">
+        {/* друзья/поиск друзей */}
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+			<Route path="/promise/:id">
+        {/* конкретная цель */}
+      </Route>
+			<Route path="/chat/:id">
+        {/* конкретный чат по конкретной цели */}
+      </Route>
+			<Route path="/user/:id">
+        {/* страница пользователя - какого-то, не нашего. Нашего должна вести на /account */}
+      </Route>
+      <Route path="/">
+				{/* Неавторизованный - на лендинг. Авторизованный - на ленту */}
+        <Main />
+      </Route>
+    </Switch>
+  );
+};
+
+export default AppSwitch;
