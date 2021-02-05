352dee5872b064469dad8136fd36401e3ab4b8b4
Danila <danilamikhaylov23@gmail.com>
2021-02-05 14:26:04 +0300

    chat add name

--- src/Components/Pages/Chat/ChatMessage.jsx
+++ src/Components/Pages/Chat/ChatMessage.jsx
@@ -5,7 +5,7 @@ import 'firebase/auth'
 import 'firebase/analytics'
 const auth = firebase.auth();
 
-const ChatMessage = ({ text, uid, photoURL }) => {
+const ChatMessage = ({ text, uid, photoURL, displayName }) => {
   
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
   
@@ -13,6 +13,7 @@ const ChatMessage = ({ text, uid, photoURL }) => {
     <>
     <div className={`message ${messageClass}`}>
       <img src={photoURL || 'https://i.pinimg.com/originals/57/7c/eb/577ceb09b2de9557a93ccb625a38c678.jpg'} />
+      <p>{displayName}</p>
       <p>{text}</p>
     </div>
   </>
