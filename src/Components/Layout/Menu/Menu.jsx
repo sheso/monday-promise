7a732b1fbaac0fe5629bf6f78828d3ab42af6d06
NikStone228 <nikitosvanilla@gmail.com>
2021-02-05 12:54:31 +0400

    auth fix and logout

--- src/Components/Layout/Menu/Menu.jsx
+++ src/Components/Layout/Menu/Menu.jsx
@@ -1,9 +1,15 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import fire from "../../../Auth/Fire";
 import { AuthContext } from "../../../Context/AuthContext";
 import "./Menu.css";
 const Menu = () => {
   const { userPhoto, currentUser } = useContext(AuthContext);
+  const history = useHistory();
+  const signOut = () => {
+    fire.auth().signOut();
+  };
+
   return (
     <div>
       <div className="dashboard">
@@ -15,28 +21,33 @@ const Menu = () => {
           />
           <h3>{currentUser}</h3>
         </div>
-        <div className="links">
-          <div className="link">
-            <img src="./images/twitch.png" alt="" />
-            <h2>
-              <Link to="/register">Регистрация</Link>
-            </h2>
+        {currentUser ? (
+          <div className="links">
+            <div className="link">
+              <img src="./images/upcoming.png" alt="" />
+              <h2>Друзья</h2>
+            </div>
+            <div className="link">
+              <img src="./images/library.png" alt="" />
+              <button onClick={() => signOut()}>Выйти</button>
+            </div>
           </div>
-          <div className="link">
-            <img src="./images/steam.png" alt="" />
-            <h2>
-              <Link to="/login">Войти</Link>
-            </h2>
+        ) : (
+          <div className="links">
+            <div className="link">
+              <img src="./images/twitch.png" alt="" />
+              <h2>
+                <Link to="/register">Регистрация</Link>
+              </h2>
+            </div>
+            <div className="link">
+              <img src="./images/steam.png" alt="" />
+              <h2>
+                <Link to="/login">Войти</Link>
+              </h2>
+            </div>
           </div>
-          <div className="link">
-            <img src="./images/upcoming.png" alt="" />
-            <h2>Друзья</h2>
-          </div>
-          <div className="link">
-            <img src="./images/library.png" alt="" />
-            <h2>Новая цель</h2>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
