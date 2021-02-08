e6aa45f2594d82de73788f9c581773ce602f91a0
NikStone228 <nikitosvanilla@gmail.com>
2021-02-08 13:31:57 +0400

    more design

--- src/Components/Pages/Chat/Chat.jsx
+++ src/Components/Pages/Chat/Chat.jsx
@@ -1,39 +1,40 @@
-import { useRef, useState, useContext } from 'react'
-import { AuthContext } from '../../../Context/AuthContext';
-import { database } from '../../../Auth/Fire';
-import 'firebase/firestore'
-import 'firebase/auth'
-import 'firebase/analytics'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import ChatMessage from './ChatMessage'
+import { useRef, useState, useContext } from "react";
+import { AuthContext } from "../../../Context/AuthContext";
+import { database } from "../../../Auth/Fire";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import ChatMessage from "./ChatMessage";
+import "./Chat.css";
 
 const Chat = () => {
-	const { currentUser } = useContext(AuthContext);
-  const scroll = useRef()
-  const messagesRef = database.messages //нужно создать коллекцию
-  const query = messagesRef.orderBy('createdAt').limit(25) //сортировка по параметру(дате создания) - лимит это ограничение по кол-во файлов
+  const { currentUser } = useContext(AuthContext);
+  const scroll = useRef();
+  const messagesRef = database.messages; //нужно создать коллекцию
+  const query = messagesRef.orderBy("createdAt").limit(25); //сортировка по параметру(дате создания) - лимит это ограничение по кол-во файлов
 
-  const [messages] = useCollectionData(query, { idField: 'id' }) //запрос по айдишникам к самой базе данных??
+  const [messages] = useCollectionData(query, { idField: "id" }); //запрос по айдишникам к самой базе данных??
 
-  const [formValue, setFormValue] = useState('')
+  const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // const { } =  здесь мы забираем данные юзера (фото, айди(имя - так как шифр) ) const { uid, photoURL } = currentUser;
-    const { uid, photoURL, displayName } = currentUser
+    const { uid, photoURL, displayName } = currentUser;
     await messagesRef.add({
       text: formValue,
       createdAt: database.getCreatedAt(),
       uid,
       displayName,
       photoURL,
-    })
-    setFormValue('')
-    scroll.current.scrollIntoView({ behavior: 'smooth' })
-  }
+    });
+    setFormValue("");
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
-    <>
+    <div className="chat">
       <main>
         {messages &&
           messages.map((message) => (
@@ -41,7 +42,7 @@ const Chat = () => {
           ))}
         <span ref={scroll} />
       </main>
-      <form onSubmit={sendMessage}>
+      <form onSubmit={sendMessage} className="chatForm">
         <input
           value={formValue}
           onChange={(event) => setFormValue(event.target.value)}
@@ -51,8 +52,8 @@ const Chat = () => {
           Отправить
         </button>
       </form>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
