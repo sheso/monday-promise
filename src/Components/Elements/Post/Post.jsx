3f8ce72ce3570fa5f972e872a479be1d50c9b4e5
NikStone228 <nikitosvanilla@gmail.com>
2021-02-10 15:31:46 +0400

    size fix

--- src/Components/Elements/Post/Post.jsx
+++ src/Components/Elements/Post/Post.jsx
@@ -32,12 +32,12 @@ const Post = ({ data, makeBet, currentUser, setForceUpdate }) => {
         </div>
         <hr style={{ width: "100%", color: "#007cc7" }} />
         <div className="card-title">
-          <div className="card-info px-3">
+          <div className="card-info px-2 py-2">
             <img
               src={data.author.photoURL}
               style={{ borderRadius: "50%", marginRight: "15px" }}
             />
-            <div className="card-info-text">
+            <div className="card-info-text ">
               <p className="card-text">{data.author.name}</p>
               <p className="card-text">{data.post.description}</p>
               <p className="card-text">{deadlineString}</p>
