d405d77ae2b1140a25fc55f8564a9262376ec924
Danila <danilamikhaylov23@gmail.com>
2021-02-11 15:22:09 +0300

    pull origin dev

--- src/Components/Elements/ProfilePost/ProfilePost.jsx
+++ src/Components/Elements/ProfilePost/ProfilePost.jsx
@@ -1,28 +1,23 @@
 import "./ProfilePost.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { finishContract } from "../../../databaseHandlers";
 import { CONTRACT_ACTIVE } from "../../../databaseHandlers";
 
 const ProfilePost = ({ data, currentUser }) => {
   const [forceUpdate, setForceUpdate] = useState(false);
-  console.log(data.post.createdAt, "hello form profile");
+
+
+  console.log(data.id, 'hello id')
 
   const deadline = new Date(data.post.deadline);
   const deadlineString = deadline.toLocaleDateString("ru-RU");
 
-  const userFinishContract = () => {
-    finishContract(data.id);
+  const userFinishContract =  (postId) => {
+    finishContract(postId);
     setForceUpdate((pre) => !pre);
-  };
-
+  }
   return (
-    // <div className="post-container my-3 mx-3">
-    //   <p>{data.post.title}</p>
-    //   <p>{data.author.name}</p>
-    //   <p>{data.post.description}</p>
-    //   <p>{deadlineString}</p>
-    //   {data.post.status === CONTRACT_ACTIVE && <button onClick={userFinishContract}>Выполнить обещание</button>}
-    //   </div>
+
     <div className="post-container my-3 mx-3">
       {data.post.status === CONTRACT_ACTIVE ? (
         <>
@@ -31,7 +26,7 @@ const ProfilePost = ({ data, currentUser }) => {
           <p>{data.author.name}</p>
           <p>{data.post.description}</p>
           <p>{deadlineString}</p>
-          <button className="lkbutton" onClick={userFinishContract}>
+          <button className="lkbutton" onClick={() => userFinishContract(data.id)}>
             Выполнить обещание
           </button>
         </>
@@ -46,6 +41,6 @@ const ProfilePost = ({ data, currentUser }) => {
       )}
     </div>
   );
-};
+}
 
-export default ProfilePost;
+export default ProfilePost
