f4a85ce7231455a0f3ce75eee5416f8b62f3d2fe
NikStone228 <nikitosvanilla@gmail.com>
2021-02-12 15:13:08 +0400

    pre release

--- src/Context/AvaArray.jsx
+++ src/Context/AvaArray.jsx
@@ -1,3 +1,12 @@
-export const avaArray = ["https://i.pinimg.com/originals/7f/2a/50/7f2a500aee5a59ea8722fcaf43d8ba09.png", 'https://img.17qq.com/images/gmchmnpcmlv.jpeg', 'https://img.17qq.com/images/kmmnlgmolcv.jpeg', 'https://img.17qq.com/images/hmgmomdkpov.jpeg', 'https://img.17qq.com/images/qgggwmhqy.jpeg' , 'https://img.17qq.com/images/kmmnlgmolpv.jpeg' , 'https://avatarfiles.alphacoders.com/123/thumb-123601.png', 'https://avatarfiles.alphacoders.com/107/thumb-107778.jpg' , 'https://lh3.googleusercontent.com/-8OQWT_1kACI2rZ3-NJP474XUjcIry8MZtIaYMr9xoVc9oCvWa4B0UFcZQf70cmCy6w=s200', 'https://lh3.googleusercontent.com/proxy/Pp-TW4FCG7VyqOfxDPx_rqBgQUHe0clEFx_qEf76W0cyuRqy5crdOwcPR9mf3mmNFuREsJ2S2k55m3qeRuHLL7pUjXksP335nzsddsutioizp_aumqHgj7xIcEPD3vj_WtxcVuYUqolCQr61mALXylp3OwiJUf5xFNJF']
-
-
+export const avaArray = [
+  "https://i.pinimg.com/originals/7f/2a/50/7f2a500aee5a59ea8722fcaf43d8ba09.png",
+  "https://img.17qq.com/images/gmchmnpcmlv.jpeg",
+  "https://img.17qq.com/images/kmmnlgmolcv.jpeg",
+  "https://img.17qq.com/images/hmgmomdkpov.jpeg",
+  "https://img.17qq.com/images/qgggwmhqy.jpeg",
+  "https://img.17qq.com/images/kmmnlgmolpv.jpeg",
+  "https://avatarfiles.alphacoders.com/123/thumb-123601.png",
+  "https://avatarfiles.alphacoders.com/107/thumb-107778.jpg",
+  "https://lh3.googleusercontent.com/-8OQWT_1kACI2rZ3-NJP474XUjcIry8MZtIaYMr9xoVc9oCvWa4B0UFcZQf70cmCy6w=s200",
+  "https://lh3.googleusercontent.com/proxy/Pp-TW4FCG7VyqOfxDPx_rqBgQUHe0clEFx_qEf76W0cyuRqy5crdOwcPR9mf3mmNFuREsJ2S2k55m3qeRuHLL7pUjXksP335nzsddsutioizp_aumqHgj7xIcEPD3vj_WtxcVuYUqolCQr61mALXylp3OwiJUf5xFNJF",
+];
